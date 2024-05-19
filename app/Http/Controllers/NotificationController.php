fdf4d1f02e118401827f5bc03038d429cafbdb59
Majumder99 <souravmajumderk99@gmail.com>
2024-05-19 14:40:02 +0600

    add the email notification

---
+++ app/Http/Controllers/NotificationController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Notifications\WelcomeNotification;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Notification;
+
+class NotificationController extends Controller {
+    public function index() {
+        $user = User::first();
+        Notification::send($user, new WelcomeNotification());
+        return response()->json(['message' => 'Notification sent!']);
+    }
+}
